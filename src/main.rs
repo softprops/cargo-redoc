e92a3346e05998442cffafcd959a37edb23c6ad9
softprops <d.tangren@gmail.com>
2020-06-05 17:15:50 -0400

    add ci

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,34 @@
 //! Cargo redoc reloads your docs in a browser when they change
 use futures_util::future;
-use http::response::Builder as ResponseBuilder;
-use http::{header, StatusCode};
-use hyper::service::{make_service_fn, service_fn};
-use hyper::{Body, Request, Response};
+use http::{header, response::Builder as ResponseBuilder, StatusCode};
+use hyper::{
+    service::{make_service_fn, service_fn},
+    Body, Request, Response, Server,
+};
 use hyper_staticfile::Static;
 use log::{debug, error, info};
-use lol_html::errors::RewritingError;
-use lol_html::html_content::ContentType;
-use lol_html::{element, rewrite_str, RewriteStrSettings};
-use notify::DebouncedEvent::*;
-use notify::RecursiveMode::*;
-use notify::Watcher;
-use std::io::Error as IoError;
-use std::path::Path;
-use std::process::Command;
-use std::sync::mpsc::channel;
-use std::thread::sleep;
-use std::time::Duration;
-use std::time::Instant;
-use tokio::sync::broadcast;
+use lol_html::{
+    element, errors::RewritingError, html_content::ContentType, rewrite_str, RewriteStrSettings,
+};
+use notify::{DebouncedEvent::*, RecursiveMode::*, Watcher};
+use std::{
+    io,
+    path::Path,
+    process::{Command, Output},
+    sync::mpsc::channel,
+    thread::sleep,
+    time::{Duration, Instant},
+};
 use structopt::StructOpt;
+use tokio::sync::broadcast;
 
+/// A hot-reloading version of `cargo doc`
 #[derive(StructOpt)]
 struct Opts {
+    /// Package to document
     #[structopt(long, short)]
-    package: Option<String>
+    package: Option<String>,
 }
-
 
 fn script() -> &'static str {
     "<script async>new EventSource('/_').onmessage = _ => location.reload();</script>"
@@ -72,22 +73,34 @@ async fn handle_request<B>(
                 .body(Body::wrap_stream(changes.into_stream()))?;
             Ok(res)
         }
-        html if html.ends_with(".html") || html.ends_with("/") => {
+        html if html.ends_with(".html") || html.ends_with('/') => {
             info!("injecting script into {}", html);
             let mut response = static_.clone().serve(req).await?;
             use hyper::body::Buf;
             let injected = inject_script(std::str::from_utf8(
                 hyper::body::aggregate(response.body_mut()).await?.bytes(),
-            )?).expect("failed to rewrite body");
-            response.headers_mut().insert("Content-Length", injected.len().to_string().parse()?);
+            )?)
+            .expect("failed to rewrite body");
+            response
+                .headers_mut()
+                .insert("Content-Length", injected.len().to_string().parse()?);
             let rewritten = Body::from(injected);
-            
+
             *response.body_mut() = rewritten;
-            
+
             Ok(response)
         }
         _ => Ok(static_.clone().serve(req).await?),
     }
+}
+
+fn rustdoc(package: &Option<String>) -> io::Result<Output> {
+    let mut cmd = Command::new("cargo");
+    cmd.args(&["doc", "--no-deps"]);
+    if let Some(p) = package {
+        cmd.args(&["-p", p.as_ref()]);
+    }
+    cmd.output()
 }
 
 #[tokio::main]
@@ -95,44 +108,45 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     pretty_env_logger::init();
     let Opts { package } = Opts::from_args();
     let start = Instant::now();
-    let doc = Command::new("cargo").args(&["doc", "--no-deps"]).spawn()?;
-    let (ctx, crx) = broadcast::channel::<String>(100);
+    rustdoc(&package)?;
+    // send file change notice to handlers
+    let (ctx, _) = broadcast::channel::<String>(100);
     let sse = ctx.clone();
     let static_ = Static::new(Path::new("target/doc/"));
-    let make_service = make_service_fn(move |_| {
+
+    let addr = ([127, 0, 0, 1], 3000).into();
+    let server = Server::bind(&addr).serve(make_service_fn(move |_| {
         let static_ = static_.clone();
         let sse = sse.clone();
         future::ok::<_, hyper::Error>(service_fn(move |req| {
             handle_request(req, static_.clone(), sse.subscribe())
         }))
-    });
+    }));
 
-    let addr = ([127, 0, 0, 1], 3000).into();
-    let server = hyper::Server::bind(&addr).serve(make_service);
-
-    let _ = doc.wait_with_output()?;
-
+    // watch sources
     let (tx, rx) = channel();
 
     let mut watcher = match notify::watcher(tx, Duration::from_secs(1)) {
         Ok(w) => w,
         Err(e) => {
-            error!("Error while trying to watch the files:\n\n\t{:?}", e);
+            error!("Error while trying to watch the files:\n\n\t{}", e);
             std::process::exit(1)
         }
     };
 
-    // Add the source directory to the watcher
+    // watch sources to know when to trigger rustdoc
     if let Err(e) = watcher.watch("src", Recursive) {
-        error!("Error while watching {:?}:\n    {:?}", "src", e);
+        error!("Error while watching src:\n    {:?}", e);
         std::process::exit(1);
     };
 
+    // watch reloadables to know when to reload browser
     if let Err(e) = watcher.watch("target/doc", Recursive) {
-        error!("Error while watching {:?}:\n    {:?}", "target/docs", e);
+        error!("Error while watching target/docs:\n    {:?}", e);
         std::process::exit(1);
     };
 
+    // watch out
     std::thread::spawn(move || loop {
         sleep(Duration::from_millis(1000));
         let paths = rx
@@ -145,30 +159,34 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 }
             })
             .collect::<Vec<_>>();
-        let (docs, srcs) = paths
+        let (reloadables, srcs) = paths
             .iter()
             .partition::<Vec<_>, _>(|path| path.to_string_lossy().contains("target/doc"));
         if !srcs.is_empty() {
             info!("src changed {:#?}", paths);
             let doc_start = Instant::now();
-            let _ = Command::new("cargo").args(&["doc", "--no-deps"]).output();
+            if let Err(e) = rustdoc(&package) {
+                error!("cargo doc run failed: {}", e);
+            }
             debug!("finished rustdoc in {:?}", Instant::now() - doc_start);
-        } else if !docs.is_empty() {
-            info!("docs changed {:#?}", paths);
+        } else if !reloadables.is_empty() {
+            info!("Reloading browsers");
+            debug!("files changed {:#?}", reloadables);
             if let Err(e) = ctx.send(format!(
                 "data:{}\n\n",
-                docs.into_iter()
+                reloadables
+                    .into_iter()
                     .map(|path| path.to_string_lossy().to_string())
                     .collect::<Vec<_>>()
                     .join(",")
             )) {
-                info!("failed to notify docs changed {:?}", e);
+                info!("Failed to notify browsers that docs changed {:?}", e);
             };
         }
     });
 
     info!("Doc server running on http://{}/", addr);
-    debug!("started in {:?}", Instant::now() - start);
+    debug!("Started in {:?}", Instant::now() - start);
     opener::open(format!("http://{}/", addr.to_string()))?;
     server.await?;
     Ok(())
